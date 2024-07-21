504957c501a0417ad75e096fb874c5cdb46d9f05
Adan Raul Morales Hernandez <adan.morales@udgvirtual.udg.mx>
2024-07-21 07:11:57 -0600

    add scripts ui
    se agregan los scripts de las ui y se agrega el game manager

---
+++ Assets/_Scripts/Ui/Applications/SupportAndHelp/SupportAndHelp.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace _Scripts.Ui.Applications.SupportAndHelp
+{
+    public class SeuppotAndHelp : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
